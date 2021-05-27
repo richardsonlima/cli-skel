f36ffc5d6877e8bb5fec8d0b260777e2ac148a9b
Richardson Lima <contatorichardsonlima@gmail.com>
2021-05-26 21:31:19 -0300

    adjustments on main

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -8,17 +10,45 @@ import (
 )
 
 func main() {
-	c := cli.NewCLI("app", "1.0.0")
-	c.Args = os.Args[1:]
-	c.Commands = map[string]cli.CommandFactory{
-		"foo": fooCommandFactory,
-		"bar": barCommandFactory,
+	log.SetOutput(ioutil.Discard)
+
+	args := os.Args[1:]
+	for _, arg := range args {
+		if arg == "-v" || arg == "version" {
+			newArgs := make([]string, len(args)+1)
+			newArgs[0] = "version"
+			copy(newArgs[1:], args)
+			args = newArgs
+			break
+		}
 	}
 
-	exitStatus, err := c.Run()
+	cli := cli.CLI{
+		Args:     args,
+		Commands: Commands,
+		HelpFunc: cli.BasicHelpFunc("cli-skel"),
+	}
+	exitStatus, err := cli.Run()
 	if err != nil {
-		log.Println(err)
+		fmt.Fprintf(os.Stderr, "Error executing CLI: %s\n", err.Error())
+		os.Exit(1)
 	}
 
 	os.Exit(exitStatus)
 }
+
+// func main() {
+// 	c := cli.NewCLI("app", "1.0.0")
+// 	c.Args = os.Args[1:]
+// 	c.Commands = map[string]cli.CommandFactory{
+// 		"foo": fooCommandFactory,
+// 		"bar": barCommandFactory,
+// 	}
+
+// 	exitStatus, err := c.Run()
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+
+// 	os.Exit(exitStatus)
+// }
